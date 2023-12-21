4f1ada68d22de93c35a74cd598e08e2e39bbd3fd
alissonoliveira1 <alissonoliveira201339@gmail.com>
2023-12-21 19:43:53 -0300

    Estudando React

---
+++ meuapp/src/App.js
@@ -0,0 +1,13 @@
+ import React from "react";
+ const Bemvindo = () => <h1>Bem Vindo</h1>
+ 
+ function App(){
+return(
+  <div>
+    <Bemvindo/>
+    oi
+    <h1>Olá Mundo</h1>
+  </div>
+)
+}
+export default App;
